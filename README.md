a8ffd9761e147d68c1d3d1df01f18be95c6e46e2
Frederic Merkl <frederic@frederics-mbp.localdomain>
2023-08-18 17:40:59 +0200

    clearGrid works

---
+++ README.md
@@ -0,0 +1,9 @@
+README
+
+This is a TheOdinProject.com project. The Goal is, to create button which creates a grid in a fixed space, based on a user Input between 1-100. The grid has always the same size. Only the elment size changes.
+On mouse hover it does draw a line within the grid.
+The Maximum amount of grid elements is 100x100.
+Atm the hover effect applies a random rgb color value to each element.
+
+
+Still trying to figure out a darkening effect which darkens the element on every interaction.
--- script.js
+++ script.js
@@ -1,49 +1,74 @@
-const body = document.querySelector("body");
-//const num = 10;
-
-const darken = function (red, green, blue, num){
-   if (num < 0) {return};
-    
-        red = red/2;
-        green = green/2;
-        blue = blue/2;
-    
-    return darken(red, green, blue, num - 1)
-}
-
-//const defineColor = function () {}
+const gridElements =  [];
+const totalGridDimension = 960;
+let randomColor = false;
+const createGridButton = document.querySelector(".createGridButton");
+const clearGridButton = document.querySelector(".clearGridButton");
+const randomColorButton = document.querySelector(".randomColorsButton");
 
 
-const applyColor = function (event) {
-    if (event.altKey){return}
-
-    const red =  Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256); 
-    event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    darken(red, green, blue, 10)
-}
-
-const createHover = function () {
-   let gridElements =  document.querySelectorAll(".grid div");
-   
-    gridElements.forEach(element => {
-        element.addEventListener("mouseover", applyColor);
+//clears the grid
+ clearGridButton.addEventListener("click", ()=> {
+    clearGrid()
 });
+
+//generates new grid when button clicked
+createGridButton.addEventListener("click", ()=> {
+    createNewGrid();
+    createHover();
+});
+
+
+randomColorButton.addEventListener("click", ()=>{
+    randomColor = !randomColor;
+    createHover()})
+
+    
+
+// creates new grid when button clicked and input given
+const createNewGrid = function() {
+    let userInput = prompt("Enter a number between 1 and 100");  
+    removeChilds();
+    if (userInput < 0 || userInput > 100) {
+        alert("Pleaser enter a number between 1 - 100")
+        userInput = 10;
+    } else{
+       return grid(userInput)
+    };  
+};
+// generate hover event listener. also removes eventlisteners before adding them
+const createHover = function () {
+    console.log("createhover-run")
+    console.log(gridElements)
+   removeEventListeners();
+   if (randomColor == false) {
+    console.log("false")
+    gridElements.forEach(element => {
+    element.addEventListener("mouseover", applyBlack)
+})} else {
+    console.log("true")
+    gridElements.forEach(element => {
+    element.addEventListener("mouseover", applyColor)
+    })
+}
 
 }
 
 const removeChilds = function () { // removes the child before creating a new grid
     const container =  document.querySelector(".grid")
-    const gridElements =  document.querySelectorAll(".grid div")
+    
+    //let gridElements =  document.querySelectorAll(".grid div")
     gridElements.forEach(element  => {
         container.removeChild(element)
     })
+    gridElements.length = 0; // to reset the nodelist array, otherwise I get a conflict between DOM and this array which do not really understand. but it makes sense.
+
 };
+
 //calculates and sets the grid elements dimensions in a predefined, fixed sized, container
 const calcElementDimension = function (num) {
-    let gridElements = document.querySelectorAll(".grid div");
-    let dimension = 960 / num;
+   
+    
+    let dimension = totalGridDimension / num;
     dimension = dimension.toString() + "px";
     gridElements.forEach(element  => {
         element.style.width = dimension;
@@ -56,37 +81,63 @@ const grid = function (num){
     for (let i = 0; i < num * num; i++) {
         const container =  document.querySelector(".grid")
         const gridElement = document.createElement("div");
-        
+        gridElements.push(gridElement)
         gridElement.classList.add("elementStyling");
         container.appendChild(gridElement);
         
     }; 
     calcElementDimension(num);
 };
-// creates new grid when button clicked and input given
-const createNewGrid = function() {
-    let userInput = prompt("Enter a number between 1 and 100");  
-    removeChilds();
-    if (userInput < 0 || userInput > 100) {
-        alert("Pleaser enter a number between 1 - 100")
-        userInput = 10;
-    } else{
-       return grid(userInput)
-    };  
+//createNewGrid(10) // calls a initial grid
+
+
+
+
+const removeEventListeners = () => {
+    console.log("remove")
+    gridElements.forEach(element => {
+        
+        if (randomColor === false){
+            console.log("colorfalse")
+            element.removeEventListener("mouseover", applyColor)}
+         else if (randomColor === true){      
+            console.log("colortrue")
+        element.removeEventListener("mouseover", applyBlack)}
+    });
 };
 
-//creates button
-//const buttonContainer = document.querySelector(".buttonContainer")
-const button = document.createElement("button");
-button.classList.add("button");
-button.textContent = "createGrid";
-body.appendChild(button);
 
-//generates new grid when button clicked
-button.addEventListener("click", ()=> {
-    createNewGrid();
-    createHover();
-});
+
+
+
+const applyBlack = (event) => {  //Event handler
+    if (event.altKey){return}
+    
+    else{event.target.style.backgroundColor = "black"};
+}
+
+const applyColor = function (event) { // event handler
+    if (event.altKey){return}
+    else {
+    const red =  Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256); 
+    event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}}
+
+
+
+function clearGrid () {
+    console.log("click")
+    gridElements.forEach(element => {
+        element.style.backgroundColor = "white";
+    })
+}
+
+
+
+
+
 
 
 
